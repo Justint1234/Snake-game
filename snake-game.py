79b1f9ffe39633a70491cdfbc8b7505d78877f07
Justin Williams <justinwilliams@Justins-MacBook-Pro.local>
2025-02-08 13:28:07 -0500

    Added directional functions

--- snake-game.py
+++ snake-game.py
@@ -1,5 +1,9 @@
 import turtle
+import time 
 
+delay = 0.1
+
+#Screen size 
 wn = turtle.Screen()
 wn.title("Snake Game By Justin")
 wn.bgcolor("green")
@@ -14,12 +18,34 @@ head.shape("square")
 head.color("black")
 head.penup()
 head.goto(0,0)
-head.direction = "stop"
+head.direction = "down"
+
+
+#Functions
+def move():
+    if head.direction == "up":
+        y = head.ycor()
+        head.sety(y + 20)
+
+    if head.direction == "down":
+        y = head.ycor()
+        head.sety(y - 20)
+
+    if head.direction == "left":
+        x = head.xcor()
+        head.setx(x - 20)
+
+    if head.direction == "right":
+        x = head.xcor()
+        head.setx(x + 20)
 
 
 #Main game loop
 while True:
     wn.update()
+    
+    move()
 
+    time.sleep(delay)
 
 wn.mainloop()
