73ad4507c3ab1641d3b0048ee5d5c53b4c12e13c
Justin Williams <justinwilliams@Justins-MacBook-Pro.local>
2025-02-08 13:05:15 -0500

    You can see the snake head

--- snake-game.py
+++ snake-game.py
@@ -3,4 +3,23 @@ import turtle
 wn = turtle.Screen()
 wn.title("Snake Game By Justin")
 wn.bgcolor("green")
-wn.setup(width=6)
+wn.setup(width=600, height=600)
+wn.tracer(0)
+
+
+# Making the snakehead
+head = turtle.Turtle()
+head.speed(0)
+head.shape("square")
+head.color("black")
+head.penup()
+head.goto(0,0)
+head.direction = "stop"
+
+
+#Main game loop
+while True:
+    wn.update()
+
+
+wn.mainloop()
