075795ee611075434e09ec6279a70a5f5ef9e240
Justin Williams <justinwilliams@Justins-MacBook-Pro.local>
2025-02-08 13:37:58 -0500

    Snake head can now  move with letters a,w,s,d

--- snake-game.py
+++ snake-game.py
@@ -18,11 +18,26 @@ head.shape("square")
 head.color("black")
 head.penup()
 head.goto(0,0)
-head.direction = "down"
+head.direction = "stop"
 
 
 #Functions
+def go_up():
+    head.direction = "up"
+
+def go_down():
+    head.direction = "down"
+
+def go_left():
+    head.direction = "left"
+
+def go_right():
+    head.direction = "right"
+
+
+
 def move():
+
     if head.direction == "up":
         y = head.ycor()
         head.sety(y + 20)
@@ -38,6 +53,16 @@ def move():
     if head.direction == "right":
         x = head.xcor()
         head.setx(x + 20)
+
+
+
+#Keyboard bindings
+wn.listen()
+wn.onkeypress(go_up, "w")
+wn.onkeypress(go_down, "s")
+wn.onkeypress(go_left, "a")
+wn.onkeypress(go_right, "d")
+
 
 
 #Main game loop
